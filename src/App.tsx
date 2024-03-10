a2f7547ed808110f6460cb790518e9f6f1028e0c
owni14 <kmwoo514@gmail.com>
2024-03-11 01:30:56 +0900

    [Style] made approximate todo list style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
 import "./app.scss";
 
 function App() {
-    return <div id="appContainer"></div>;
+    return (
+        <div id="appContainer">
+            <div className="todo-list-wrapper">
+                <div className="todo-list-top">
+                    <div className="top-wrapper">
+                        <p>section1</p>
+                        <p>section2</p>
+                        <p>section3</p>
+                    </div>
+                </div>
+                <section className="todo-list-section-wrapper">
+                    <div className="input-wrapper">
+                        <input type="text" />
+                        <button>Add</button>
+                    </div>
+                    <main className="list-wrapper">
+                        <div className="detail-list-wrapper">
+                            <p>text</p>
+                            <p>delet button</p>
+                        </div>
+                        <div className="detail-list-wrapper">
+                            <p>text</p>
+                            <p>delet button</p>
+                        </div>
+                        <div className="detail-list-wrapper">
+                            <p>text</p>
+                            <p>delet button</p>
+                        </div>
+                        <div className="detail-list-wrapper">
+                            <p>text</p>
+                            <p>delet button</p>
+                        </div>
+                    </main>
+                </section>
+            </div>
+        </div>
+    );
 }
 
 export default App;
